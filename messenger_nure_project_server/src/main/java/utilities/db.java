721ea0a25770a1a767bd100b3f7fc3d0394f3bac
volodymyrMakhotilo <vkmahotilo@gmail.com>
2022-07-12 11:51:22 +0300

    add hiberanete, establish db connection

---
+++ messenger_nure_project_server/src/main/java/utilities/db.java
@@ -0,0 +1,30 @@
+package utilities;
+
+//import models.Auto;
+//import models.User;
+import models.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class db{
+    private static SessionFactory sessionFactory;
+
+    private db() {}
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration().configure();
+                configuration.addAnnotatedClass(User.class);
+                //configuration.addAnnotatedClass(Auto.class);
+                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+                sessionFactory = configuration.buildSessionFactory(builder.build());
+
+            } catch (Exception e) {
+                System.out.println("Исключение!" + e);
+            }
+        }
+        return sessionFactory;
+    }
+}
