5c0d085494eb6e50ef4dfad2206db76013217ccf
volodymyrMakhotilo <vkmahotilo@gmail.com>
2022-07-13 11:43:55 +0300

    add basic UI #5

---
+++ messenger_nure_project_client/src/main/java/com/example/messenger_nure_project_client/Controller.java
@@ -0,0 +1,14 @@
+package com.example.messenger_nure_project_client;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
