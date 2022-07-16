51836d07417e0837821735f82e5e3dd212668b32
volodymyrMakhotilo <vkmahotilo@gmail.com>
2022-07-16 18:34:29 +0300

    add messenger like interface #5

--- messenger_nure_project_client/src/main/java/com/example/messenger_nure_project_client/Application.java
+++ messenger_nure_project_client/src/main/java/com/example/messenger_nure_project_client/Application.java
@@ -9,14 +9,21 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("main_view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Messenger!");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
+        try {
+            ClientSocket clientSocket = new ClientSocket();
+            new Thread(clientSocket).start();
+
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
         launch();
     }
 }
--- messenger_nure_project_client/src/main/java/com/example/messenger_nure_project_client/Controller.java
+++ messenger_nure_project_client/src/main/java/com/example/messenger_nure_project_client/Controller.java
@@ -2,13 +2,25 @@ package com.example.messenger_nure_project_client;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 public class Controller {
     @FXML
-    private Label welcomeText;
+    private Label testLabel;
+    @FXML
+    private TextField message_field;
+    @FXML
+    private VBox incoming_filed;
+    @FXML
+    private VBox outgoing_filed;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onSendButtonPress() {
+        outgoing_filed.getChildren().add(new Label(message_field.getText()));
+    }
+    @FXML
+    protected void onMessageIncome(){
+
     }
 }
