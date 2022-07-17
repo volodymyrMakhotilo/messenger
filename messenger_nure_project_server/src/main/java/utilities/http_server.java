47a55000d0522abcbd37fc637a09faa85de912f7
volodymyrMakhotilo <vkmahotilo@gmail.com>
2022-07-18 00:31:08 +0300

    send all users #12

--- messenger_nure_project_server/src/main/java/utilities/http_server.java
+++ messenger_nure_project_server/src/main/java/utilities/http_server.java
@@ -1,21 +1,29 @@
 package utilities;
 
 import layers.Service;
+import models.User;
+import xml.XML;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class http_server {
+    private static XML xml;
     static Service service;
+    static XML_Marshaller xmlMarshaller;
+    private static ArrayList<SocketProcessor> sockets = new ArrayList<>();
 
     public http_server(Service service) {
         try {
+            xml = new XML();
+            xmlMarshaller = new XML_Marshaller();
             this.service = service;
             createServer();
         } catch (Throwable e) {
@@ -32,11 +40,13 @@ public class http_server {
             Socket s = ss.accept();
 
             num++;
-
-
             System.err.println("Client " + num + " is here!");
+
             SocketProcessor socket = new SocketProcessor(s);
-            socket.processLogin(s.getInetAddress(), s.getPort());
+
+            socket.processLogin(xmlMarshaller);
+
+            sockets.add(socket);
             new Thread(socket).start();
         }
     }
@@ -58,7 +68,7 @@ public class http_server {
                     System.out.println("Connection established");
                     readRequest();
                     System.out.println("Response gotten");
-                    writeResponse();
+                    writeResponse("200");
                     System.out.println("Request sent");
                 }
             } catch (Throwable t) {
@@ -73,12 +83,9 @@ public class http_server {
             System.err.println("Client processing finished");
         }
 
-        public void writeResponse() throws Throwable {
-            for (int i = 0; i < 10; i++) {
-                String response = "Hi, client\n";
-                os.write(response);
-                os.flush();
-            }
+        public void writeResponse(String response) throws Throwable {
+            os.write(response + "\n");
+            os.flush();
         }
 
         public String readRequest() throws Throwable {
@@ -88,16 +95,31 @@ public class http_server {
             return word;
         }
 
-        public void processLogin(InetAddress ip, int port) throws Throwable {
-            String login = readRequest();
-            if (service.checkUser(login)) {
-
+        public void processLogin(XML_Marshaller XML_Marshaller) throws Throwable {
+            String request = readRequest();
+            User user = XML_Marshaller.parseUser(request);
+            if (service.checkUser(user.getLogin())) {
+                updateUsers(user);
             } else {
-                service.addUser(login, ip.toString(), port);
+                service.addUser(user.getLogin(), user.getIp(), user.getPort());
             }
-            writeResponse();
+            System.out.println("Gonna answer on Log!");
+            writeResponse(getUsers());
         }
+    }
 
+    public static String getUsers() {
+        String message = "";
+        List<User> users = service.getUsers();
+            for (User user : users) {
+                message += xml.getUser(user.getLogin(), user.getIp(), user.getPort());
+            }
+        return xml.wrapMessage(message);
+    }
 
+    public static void updateUsers(User user) throws Throwable {
+        for (SocketProcessor socket : sockets) {
+            socket.writeResponse(xml.getUser(user.getLogin(), user.getIp(), user.getPort()));
+        }
     }
 }
